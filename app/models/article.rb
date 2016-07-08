a89020b6b5ee38a62562b5d66e4382ef0745627b
Tiago Arrosi <twarrosi@gmail.com>
2016-07-07 21:09:15 -0300

    add association beetwen user and articles and added debbuger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
-class Article < ActiveRecord::Base
+class Article <   ActiveRecord::Base
+
+  belongs_to :user
 
   validates :title, presence: true
   validates :description, presence: true
-
+  validates :user_id, presence: true
 end
