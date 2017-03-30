822cfc761835fce53905fa1ac5616f888983d4d4
Tiago Arrosi <twarrosi@gmail.com>
2017-03-29 21:37:31 -0300

    Article and category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,9 @@ class Article <   ActiveRecord::Base
 
   belongs_to :user
 
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+
   validates :title, presence: true
   validates :description, presence: true
   validates :user_id, presence: true
